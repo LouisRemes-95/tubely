aa16060d62fc437ed7790fb1751b244497485c62
Louis Remes <louisremes@hotmail.be>
2025-10-29 19:02:37 +0100

    started adding fast start

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -173,3 +173,13 @@ func getVideoAspectRatio(filePath string) (string, error) {
 		return "other", nil
 	}
 }
+
+func processVideoForFastStart(filePath string) (string, error) {
+	outputFilePath := filePath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", outputFilePath)
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("failed to run command: %w", err)
+	}
+	return outputFilePath, nil
+}
